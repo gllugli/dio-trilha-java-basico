f7da3f345ef445cfaf92d57f14cdba9fc28e5a9c
Gabriel Lugli <gabriel.lugli@gmail.com>
2025-03-15 00:41:19 -0300

    Alteracao

--- Estudos/Pilares-POO/src/estudo_interface/equipamentos/estabelecimento/Fabrica.java
+++ Estudos/Pilares-POO/src/estudo_interface/equipamentos/estabelecimento/Fabrica.java
@@ -1,11 +1,13 @@
 package estudo_interface.equipamentos.estabelecimento;
 
 import estudo_interface.equipamentos.impressora.Impressora;
-import estudo_interface.equipamentos.impressora.Laserjet;
+// import estudo_interface.equipamentos.impressora.Laserjet;
+// import estudo_interface.equipamentos.impressora.Deskjet;
+import estudo_interface.equipamentos.multifuncional.EquipamentoMultifuncional;
 
 public class Fabrica {
     public static void main(String[] args) {
-        Impressora impressora = new Laserjet();
+        Impressora impressora = new EquipamentoMultifuncional();
 
         impressora.imprimir();
     }
